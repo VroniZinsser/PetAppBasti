22cbed6383bb6798139e31ee35ab9d5d6a8e47f6
cristian-corbalan <ccorbalandw@gmail.com>
2021-09-25 01:15:31 -0300

    Agregar migration y seeder para la tabla pets

---
+++ mascotas-api/database/migrations/2021_09_24_212333_create_pets_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 30);
+            $table->string('breed', 45)->nullable();
+            $table->string('temperament', 45)->nullable();
+            $table->boolean('neutered')->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->softDeletes();
+            $table->unsignedBigInteger('images_id');
+            $table->unsignedTinyInteger('sexes_id');
+            $table->unsignedTinyInteger('species_id');
+            $table->foreign('images_id')->references('id')->on('images');
+            $table->foreign('sexes_id')->references('id')->on('sexes');
+            $table->foreign('species_id')->references('id')->on('species');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pets', function (Blueprint $table){
+           $table->dropForeign(['images_id']);
+           $table->dropForeign(['sexes_id']);
+           $table->dropForeign(['species_id']);
+        });
+        Schema::dropIfExists('pets');
+    }
+}
---
+++ mascotas-api/database/seeders/PetsSeeder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PetsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('pets')->insert([
+            [
+                'id' => 1,
+                'name' => 'Sisi',
+                'breed' => null,
+                'temperament' => 'Mimosa, le gusta hacer renegar al dueño',
+                'neutered' => 1,
+                'date_of_birth' => now(),
+                'images_id' => 2,
+                'sexes_id' => 1,
+                'species_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 2,
+                'name' => 'Ishi',
+                'breed' => null,
+                'temperament' => null,
+                'neutered' => null,
+                'date_of_birth' => null,
+                'images_id' => 2,
+                'sexes_id' => 1,
+                'species_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => 3,
+                'name' => 'Dadi',
+                'breed' => 'American bully',
+                'temperament' => null,
+                'neutered' => null,
+                'date_of_birth' => null,
+                'images_id' => 1,
+                'sexes_id' => 2,
+                'species_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
