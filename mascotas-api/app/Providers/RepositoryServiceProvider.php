48b687054d66354dc7cc8040db772a05c8b5dc09
cristian-corbalan <ccorbalandw@gmail.com>
2021-10-10 09:49:01 -0300

    Crear repositorio y servicio para imágenes con funcionalidad de subir archivos y guardar en la base de datos

--- mascotas-api/app/Providers/RepositoryServiceProvider.php
+++ mascotas-api/app/Providers/RepositoryServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\ImageRepository;
 use App\Repositories\SexRepository;
 use App\Repositories\SpeciesRepository;
+use App\Services\ImageService;
 use App\Services\SexService;
 use App\Services\SpeciesService;
 use Illuminate\Support\ServiceProvider;
@@ -12,6 +14,7 @@ class RepositoryServiceProvider extends ServiceProvider
 {
     public $bindings = [
         SexRepository::class => SexService::class,
-        SpeciesRepository::class => SpeciesService::class
+        SpeciesRepository::class => SpeciesService::class,
+        ImageRepository::class => ImageService::class,
     ];
 }
