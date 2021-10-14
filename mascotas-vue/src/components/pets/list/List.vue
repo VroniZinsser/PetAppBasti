d63cec2fccacf3f846033f581d5eefa7debb14ac
cristian-corbalan <ccorbalandw@gmail.com>
2021-10-13 21:46:27 -0300

    Crear vista y componente básicos

---
+++ mascotas-vue/src/components/pets/list/List.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "List"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ mascotas-vue/src/views/pets/ShowList.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <h1>Tus mascotas</h1>
+
+    <List></List>
+  </v-container>
+</template>
+
+<script>
+import List from "../../components/pets/list/List";
+
+export default {
+  name: "ShowList",
+  components: {List}
+}
+</script>
+
+<style scoped>
+
+</style>
