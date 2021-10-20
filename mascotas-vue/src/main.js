7e1970806085c0c9f9bb3b1267a741d6bf883179
VroniZinsser <vroni.z@posteo.de>
2021-10-20 21:06:49 +0200

    Deshacer cambios de google maps (no funcionó bien)

--- mascotas-vue/src/main.js
+++ mascotas-vue/src/main.js
@@ -2,18 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import VueGeolocation from 'vue-browser-geolocation'
-import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
-Vue.use(VueGeolocation)
-
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyAx0l4CqJy3P2widDbKV-TyR3QM0ySLCDc'
-  },
-  installComponents: true,
-})
 
 new Vue({
   router,
--- mascotas-vue/src/views/map/ShowMap.vue
+++ mascotas-vue/src/views/map/ShowMap.vue
@@ -1,19 +1,8 @@
 <template>
   <v-container fluid>
-    <h1>Mapa</h1>
-    <h2>Tus coordinatas: </h2>
-    <p>Latitude: {{ coordinates.lat }}, Longitude: {{ coordinates.lng }}</p>
-
-    <h2>Here Maps</h2>
     <Map 
       ref="hereMap"
     />
-
-    <h2>Vue2-google-maps</h2>
-    <GmapMap
-      :center="coordinates"
-      :zoom="14"
-      style="width: 640px; height:360px; margin:32px auto;"></GmapMap>
 
   </v-container>
 
@@ -30,20 +19,8 @@ export default {
     
   },
   data: () => ({
-    coordinates: {
-      lat: 0,
-      lng:0
-    }
     
   }),
-
-  created() {
-    this.$getLocation({})
-      .then(coordinates => {
-        this.coordinates = coordinates;
-      })
-      .catch(error => alert(error));
-  },
 
   mounted() {
     
