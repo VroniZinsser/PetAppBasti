10a092e449cf72cb44658dfd3148a235bcfb0095
VroniZinsser <vroni.z@posteo.de>
2021-10-22 16:18:42 +0200

    Sacar archivo redundante de constants y agregar API_KEY

--- mascotas-vue/src/constant/index.js
+++
@@ -1,3 +0,0 @@
-const API = "http://127.0.0.1:8000/api/";
-
-export {API};
--- mascotas-vue/src/constants/index.js
+++ mascotas-vue/src/constants/index.js
@@ -1,3 +1,7 @@
 const API = "http://127.0.0.1:8000/api/";
+const HEREMAPS_API_KEY = "I8Jxzv6xtyMgJodcYgvnsbWiuhjAN6zZhEv0_IQ4Ej8";
 
-export {API};
+export { 
+    API, 
+    HEREMAPS_API_KEY
+};
