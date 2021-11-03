25302dd85546657ab61b07383654279b1e38054a
VroniZinsser <vroni.z@posteo.de>
2021-11-03 14:14:45 +0100

    Agregar migration de peso

---
+++ mascotas-api/database/migrations/2021_09_24_212334_create_weights_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weights', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date_time');
+            $table->unsignedSmallInteger('weight');
+            $table->unsignedBigInteger('pets_id');
+            $table->foreign('pets_id')->references('id')->on('pets');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('weights', function (Blueprint $table){
+           $table->dropForeign(['pets_id']);
+        });
+        Schema::dropIfExists('weights');
+    }
+}
