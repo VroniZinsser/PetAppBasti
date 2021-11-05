8fc6adc0b65cda6483b896e4e4424bf6a8a4e09d
VroniZinsser <vroni.z@posteo.de>
2021-11-05 17:05:23 +0100

    cambiar fecha_hora a fecha

--- mascotas-vue/src/components/pets/weight/Form.vue
+++ mascotas-vue/src/components/pets/weight/Form.vue
@@ -13,7 +13,7 @@
             required
             type="number"
         ></InputText>
-        <v-datetime-picker label="Select Datetime" v-model="formData.date_time"> </v-datetime-picker>
+        <v-date-picker v-model="formData.date"></v-date-picker>
     </v-form>
 </template>
 <script>
@@ -22,34 +22,36 @@ import InputText from "@/components/general/inputs/InputText";
 export default {
   name: "Form",
   components: {
-        InputText,
-    },
-  data: () => ({
-    loading: false,
-    formData: {
-      weight: null,
-      date_time: null,
-      date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-    },
+    InputText,
+  },
+  props: {
+    dateTime: String
+  },
+  data: function() {
+    return {
+      loading: false,
+      formData: {
+        weight: null,
+        date: this.getCurrentDate(),
+      },
 
-    errors: {
-      weight: null,
-      date_time: null,
-    },
-    rules: {
-      obligatory: v => !!v || 'Este campo es obligatorio.',
-      numeric: value => !isNaN(value) || 'El valor debe ser numérico.',
-    },
-    methods: {
-    currentDateTime() {
-        const current = new Date();
-        const date = current.getFullYear()+'-'+(current.getMonth()+1)+'-'+current.getDate();
-        const time = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
-        const dateTime = date +' '+ time;
-        
-        return dateTime;
-        }
+      errors: {
+        weight: null,
+        date: null,
+      },
+      rules: {
+        obligatory: v => !!v || 'Este campo es obligatorio.',
+        numeric: value => !isNaN(value) || 'El valor debe ser numérico.',
+      }
     }
-  })
+  },
+
+   methods: {
+    getCurrentDate() {
+      const current = new Date();
+      const date = current.getFullYear() + '-' + (current.getMonth()+1) + '-' + current.getDate();
+      return date;
+    }
+  },
 }
 </script>
