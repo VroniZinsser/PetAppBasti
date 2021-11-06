19042cd742e97f4fb0dec83a30ac7d5d96469a95
VroniZinsser <vroni.z@posteo.de>
2021-11-07 00:18:31 +0100

    Eliminar datetime-picker

--- mascotas-vue/src/main.js
+++ mascotas-vue/src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import DatetimePicker from 'vuetify-datetime-picker'
 
 Vue.config.productionTip = false
-Vue.use(DatetimePicker);
 
 new Vue({
   router,
