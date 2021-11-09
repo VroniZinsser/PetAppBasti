cb964f27ae339d865ba4cc8ab0ee7daf318b1235
cristian-corbalan <ccorbalandw@gmail.com>
2021-11-09 17:51:33 -0300

    Agregar enlace para saltar contenido y estilos del botón

--- mascotas-vue/src/App.vue
+++ mascotas-vue/src/App.vue
@@ -1,43 +1,14 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="primary"
-      dark
+        app
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+      <div id="skip-to-content">
+        <a href="#main-content">Saltar al contenido principal</a>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
-    <v-main>
+    <v-main id="main-content">
       <router-view/>
     </v-main>
   </v-app>
@@ -49,7 +20,6 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
   }),
 };
 </script>
