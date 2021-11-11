615c889616cbcbae96f0e86552bfc3021b41a115
cristian-corbalan <ccorbalandw@gmail.com>
2021-11-11 11:26:45 -0300

    Crear estructura de la barra de navegación
    Agregar logo de la marca a la barra de navegación y que sea un enlace (actualmente lleva a la home)
    Agregar enlaces a la barra de navegación (los enlaces de agenda y conversaciones llevan a la home)
    Crear una modal para el enlace de configuración y cerrar la sesión del usuario (los enlaces sin funcionalidad)
    Crear función para que aparezca y desaparezca la ventana modal

--- mascotas-vue/src/App.vue
+++ mascotas-vue/src/App.vue
@@ -1,12 +1,46 @@
 <template>
   <v-app>
-    <v-app-bar
-        app
-    >
-      <div id="skip-to-content">
-        <a href="#main-content">Saltar al contenido principal</a>
+    <div id="skip-to-content">
+      <a href="#main-content">Saltar al contenido principal</a>
+    </div>
+
+    <v-app-bar app>
+      <div id="brand-image">
+        <router-link :to="{name: 'Home'}">
+          <img :src="getCompletePath('brand/logotype.png')" alt="Logo de Basti">
+        </router-link>
       </div>
+
+      <v-spacer></v-spacer>
+
+      <ul id="nav-links">
+        <li v-for="link in routerLinks" :key="link.text">
+          <router-link :to="{name: link.name}">
+            <span class="material-icons">{{ link.icon }}</span>
+            <span>{{ link.text }}</span>
+          </router-link>
+        </li>
+        <li>
+          <a href="#" @click.prevent="dialog = true">
+            <span class="material-icons">account_circle</span>
+            <span class="sr-only">Abrir opciones (abre ventana modal)</span>
+          </a>
+        </li>
+      </ul>
     </v-app-bar>
+
+    <!--Config modal-->
+    <v-dialog v-model="dialog" max-width="500" class="container--fluid">
+      <v-card id="config-list">
+        <ul>
+          <li><a href="#">Configuración</a></li>
+          <li><a href="#" class="danger-text">Cerrar sesión</a></li>
+          <li>
+            <v-btn @click="dialog=false">Cancelar</v-btn>
+          </li>
+        </ul>
+      </v-card>
+    </v-dialog>
 
     <v-main id="main-content">
       <router-view/>
@@ -16,10 +50,40 @@
 
 <script>
 
+import {PATH_IMG} from "./constants";
+
 export default {
   name: 'App',
 
   data: () => ({
+    dialog: true,
+    routerLinks: [
+      {
+        name: 'Home',
+        text: 'Agenda',
+        icon: 'event',
+      },
+      {
+        name: 'ShowMap',
+        text: 'Explorar',
+        icon: 'search',
+      },
+      {
+        name: 'PetShowList',
+        text: 'Mascotas',
+        icon: 'format_list_bulleted',
+      },
+      {
+        name: 'Home',
+        text: 'Conversaciones',
+        icon: 'chat_bubble',
+      },
+    ]
   }),
+  methods: {
+    getCompletePath(path) {
+      return PATH_IMG + path
+    }
+  }
 };
 </script>
