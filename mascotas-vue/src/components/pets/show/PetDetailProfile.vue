7ab649a96241a348c0f096afa0c7467436e43d71
VroniZinsser <vroni.z@posteo.de>
2021-11-12 03:00:16 +0100

    Contenidos del perfil de mascota

--- mascotas-vue/src/components/pets/show/PetDetailProfile.vue
+++ mascotas-vue/src/components/pets/show/PetDetailProfile.vue
@@ -1,5 +1,37 @@
 <template>
+  <div>
     <p>{{ pet.name }}</p>
+    <ul>
+      <li>
+        <span>Nombre:</span>
+        <span>{{ pet.name }}</span>
+      </li>
+      <li>
+        <span>Especie:</span>
+        <span>{{ pet.species.name }}</span>
+      </li>
+      <li v-if="pet.breed">
+        <span>Raza:</span>
+        <span>{{ pet.breed }}</span>
+      </li>
+      <li>
+        <span>Castrado:</span>
+        <span>{{ pet.neutered ? 'Sí' : 'No'}}</span>
+      </li>
+      <li>
+        <span>Sexo:</span>
+        <span>{{ pet.sex.name }}</span>
+      </li>
+      <li v-if="pet.date_of_birth">
+        <span>Fecha de nacimiento:</span>
+        <span>{{ pet.date_of_birth }}</span>
+      </li>
+      <li v-if="pet.temperament">
+        <span>Temperamento:</span>
+        <span>{{ pet.temperament }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -10,6 +42,6 @@ export default {
       type: Object,
       required: true
     },
-  }
+  },
 }
 </script>
