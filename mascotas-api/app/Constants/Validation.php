e4524e159c739d20ab689190fa1caf03693b3184
VroniZinsser <vroni.z@posteo.de>
2021-11-15 13:32:50 +0100

    Cambiar de constantes a metodos estaticos

--- mascotas-api/app/Constants/Validation.php
+++ mascotas-api/app/Constants/Validation.php
@@ -3,22 +3,42 @@
 namespace App\Constants;
 
 class Validation {
-    const REQUIRED  = 'required';
-    const MAX_CARACTERS = 'max_caracters';
-    const MAX_SIZE = 'max_size';
-    const VALID_VALUE = 'valid_value';
-    const BEFORE_EQUAL = 'before_equal';
 
     private static $messages = [
-        self::REQUIRED => 'Por favor ingresá :field',
-        self::MAX_CARACTERS => ':field no puede contener más de :value caracteres',
-        self::VALID_VALUE => 'Por favor ingresá un valor válido para :field',
-        self::MAX_SIZE => ':field no puede pesar más de :value',
-        self::BEFORE_EQUAL => 'Por favor ingresá :field anterior o igual a :value',
+        'required' => 'Por favor ingresá :field',
+        'max_caracters' => ':field no puede contener más de :max caracteres',
+        'min_caracters' => ':field no puede contener menos de :min caracteres',
+        'valid_value' => ':field que ingesaste no es :valid',
+        'max_size' => ':field no puede pesar más de :max',
+        'before_equal' => 'Por favor ingresá :field anterior o igual a :date',
     ];
 
     public static function get($message, $field, ?string $value='') {
         return str_replace([':field', ':value'], [$field, $value], self::$messages[$message]);
  
+    }
+
+    public static function msg_required($field) {
+        return str_replace(':field', $field, self::$messages['required']);
+    }
+
+    public static function msg_max_caracters($field, $max) {
+        $fieldCapitalize = ucfirst($field);
+        return str_replace([':field', ':max'], [$fieldCapitalize, $max], self::$messages['max_caracters']);
+    }
+
+    public static function msg_valid_value($field, $fieldIsFeminine = false) {
+        $fieldCapitalize = ucfirst($field);
+        $valid = $fieldIsFeminine ? 'válida' : 'válido';
+        return str_replace([':field', ':valid'], [$fieldCapitalize, $valid], self::$messages['valid_value']);
+    }
+
+    public static function msg_max_size($field, $max) {
+        $fieldCapitalize = ucfirst($field);
+        return str_replace([':field', ':max'], [$fieldCapitalize, $max], self::$messages['max_size']);
+    }
+
+    public static function msg_before_equal($field, $date) {
+        return str_replace([':field', ':date'], [$field, $date], self::$messages['before_equal']);
     }
 }
