b9c2db5fd7f385a3ba1e3489bb5b89c3b24281c3
cristian-corbalan <ccorbalandw@gmail.com>
2021-11-19 17:52:14 -0300

    Cambios minimos en los componentes de la sección general y geolocation

--- mascotas-vue/src/components/general/inputs/InputText.vue
+++ mascotas-vue/src/components/general/inputs/InputText.vue
@@ -1,37 +1,37 @@
 <template>
-    <v-text-field
-        class="form-control"
-        v-bind="$attrs"
-        v-on="$listeners"
-        :type="$attrs.type ? $attrs.type : 'text'"
-        :id="identifier"
-        :name="identifier"
-        :label="$attrs.label + ($attrs.required === '' ? ' *' : '')"
-        :value="value" 
-        :error="errors !== null"
-        :messages="errors ? errors[0] : ''"
-        :disabled="loading"
-    ></v-text-field>
-    
+  <v-text-field
+      class="form-control"
+      v-bind="$attrs"
+      v-on="$listeners"
+      :type="$attrs.type ? $attrs.type : 'text'"
+      :id="identifier"
+      :name="identifier"
+      :label="$attrs.label + ($attrs.required === '' ? ' *' : '')"
+      :value="value"
+      :error="errors !== null"
+      :messages="errors ? errors[0] : ''"
+      :disabled="loading"
+  ></v-text-field>
+
 </template>
 
 <script>
 
 export default {
-    name: "InputText",
+  name: "InputText",
 
-    props: {
-        value: {},
-        identifier: {
-            type: String,
-        },
-        errors: {
-            default: null
-        },
-        loading: {
-            type: Boolean,
-            default: false
-        }
+  props: {
+    value: {},
+    identifier: {
+      type: String,
     },
+    errors: {
+      default: null
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
 }
 </script>
