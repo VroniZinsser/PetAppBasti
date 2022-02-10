748117ddf4203933caed50c5c6108bdc37ceb9c7
cristian-corbalan <ccorbalandw@gmail.com>
2022-02-09 21:11:39 -0300

    Instalar IDE Helper Generator y generar documentación

--- mascotas-api/app/Models/Hour.php
+++ mascotas-api/app/Models/Hour.php
@@ -5,6 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Hour
+ *
+ * @property int $id
+ * @property string $time
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Medicine[] $medicines
+ * @property-read int|null $medicines_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Hour newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Hour newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Hour query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Hour whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Hour whereTime($value)
+ * @mixin \Eloquent
+ */
 class Hour extends Model
 {
     use HasFactory;
--- mascotas-api/app/Models/Vaccine.php
+++ mascotas-api/app/Models/Vaccine.php
@@ -6,6 +6,27 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * App\Models\Vaccine
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $date
+ * @property int $pets_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Pet $pet
+ * @method static \Illuminate\Database\Eloquent\Builder|Vaccine newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Vaccine newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Vaccine query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Vaccine whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vaccine whereDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vaccine whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vaccine whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vaccine wherePetsId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Vaccine whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Vaccine extends Model
 {
     use HasFactory;
