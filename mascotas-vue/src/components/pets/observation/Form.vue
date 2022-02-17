e8cafdd4b2ebc5809d5a27bc7d82a955d7442f33
VroniZinsser <vroni.z@posteo.de>
2022-02-17 14:42:34 +0100

    Agregar botón, dialogo y estilos

--- mascotas-vue/src/components/pets/observation/Form.vue
+++ mascotas-vue/src/components/pets/observation/Form.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <v-form
       action="observaciones/crear"
       method="post"
@@ -17,10 +18,22 @@
 
     <button class="main-btn" type="submit" :disabled="loading">Guardar</button>
   </v-form>
+  <button class="delete-btn" :disabled="loading" @click="showWarnDialog = true" >Borrar nota</button>
+  <WarnDialog
+    :showDialog="showWarnDialog" 
+    dialogTitle="Borrar nota"
+    dialogText="Podrás escribir una nueva nota cuando quieras"
+    acceptButtonText="Borrar"
+
+    @cancle="showWarnDialog = false"
+    @accept="deleteObservation()"
+  />
+</div>
 </template>
 
 <script>
 import Textarea from "@/components/general/inputs/Textarea";
+import WarnDialog from "@/components/general/notifications/WarnDialog";
 import petServices from "../../../services/pets";
 import store from "@/store";
 
@@ -28,6 +41,7 @@ export default {
   name: "Form",
   components: {
     Textarea,
+    WarnDialog,
   },
   props: {
     pet_id: {
@@ -42,6 +56,7 @@ export default {
     return {
       loading: false,
       store,
+      showWarnDialog: false,
       formData: {
         observation: null,
       },
@@ -80,19 +95,37 @@ export default {
                   });
                 } else {
                   this.store.setStatus({
-                    msg: 'Algo salió mal. La observación no se guardó correctamente.',
+                    msg: 'Algo salió mal. La nota no se guardó correctamente.',
                     type: 'error',
                   });
                 }
               } else {
                 this.store.setStatus({
-                  msg: '¡La observación está guardada!',
+                  msg: '¡La nota está guardada!',
                   type: 'success',
                 });
                 this.$router.push({name: 'Pets'});
               }
             })
       }
+    },
+
+    deleteObservation() {
+      petServices.deleteObservation()
+        .then(res => {
+          if (!res.success) {
+            this.store.setStatus({
+              msg: 'Algo salió mal. Intentalo nuevamente más tarde.',
+              type: 'error',
+            });
+          } else {
+            this.store.setStatus({
+              msg: 'La nota se borró correctamente.',
+              type: 'success',
+            });
+            this.$router.push({name: 'Pets'});
+          }
+        })
     }
   },
   mounted() {
