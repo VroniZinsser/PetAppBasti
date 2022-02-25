a2b52dcd4f4e66a6c8251a0cfce09c53ab4677b2
cristian-corbalan <ccorbalandw@gmail.com>
2022-02-25 19:48:17 -0300

    Hacer que el componente también funcione para usuarios dueños

--- mascotas-vue/src/components/contact/ShareListSubItem.vue
+++ mascotas-vue/src/components/contact/ShareListSubItem.vue
@@ -1,17 +1,17 @@
 <template>
   <v-expansion-panel>
     <WarnDialog
-      :showDialog="showWarnDialog" 
-      :dialogTitle="`¿Dejar de compartir ${pet.name}?`"
-      :dialogText="`${professionalName} no podrá ver más el perfil de ${pet.name}. Podés volver a compartir la mascota cuando quieras.`"
-      acceptButtonText="Dejar de compartir"
+        :showDialog="showWarnDialog"
+        :dialogTitle="dialogTitle"
+        :dialogText="dialogText"
+        :acceptButtonText="dialogButtonText"
 
-      @cancel="showWarnDialog = false"
-      @accept="$parent.$parent.$emit('delete-request', request.id)"
+        @cancel="showWarnDialog = false"
+        @accept="$parent.$parent.$emit('delete-request', request.id)"
     />
     <v-expansion-panel-header>
       <div>
-        <img :src="createImgPath(pet.image.src)" :alt="pet.image.alt" />
+        <img :src="createImgPath(pet.image.src)" :alt="pet.image.alt"/>
         <div class="pet-container">
           <p class="pet-name">{{ pet.name }}</p>
           <p class="expiration">{{ expirationText }}</p>
@@ -20,20 +20,20 @@
     </v-expansion-panel-header>
     <v-expansion-panel-content>
       <div v-if="request.description" class="quote-container">
-        <p class="quote-intro">Tu mensaje a {{professionalName}}:</p>
+        <p class="quote-intro">{{ quoteIntro }}</p>
         <p class="quote">"{{ request.description }}"</p>
       </div>
       <button @click="showWarnDialog = true">
-        <v-icon> mdi-link-variant-off </v-icon>
-        Dejar de compartir
+        <v-icon> mdi-link-variant-off</v-icon>
+        {{ this.professionalName ? 'Dejar de compartir' : 'Dejar de ver' }}
       </button>
     </v-expansion-panel-content>
   </v-expansion-panel>
 </template>
 
 <script>
-import { createImgPath } from "@/helpers";
-import { formatDate } from "@/helpers";
+import {createImgPath} from "@/helpers";
+import {formatDate} from "@/helpers";
 import WarnDialog from "@/components/general/notifications/WarnDialog";
 
 
@@ -46,24 +46,11 @@ export default {
     },
     professionalName: {
       type: String,
-      required: true
+      default: null,
     }
   },
   components: {
     WarnDialog,
-  },
-  computed: {
-    pet() {
-      return this.request.pet;
-    },
-    /**
-     * Builds a sentence about expiration with formatted date, based on the pet's sex
-     */
-    expirationText() {
-      let text = this.pet.sexes_id === 1 ? "Compartida" : "Compartido";
-      text += " hasta el " + this.formatDate(this.request.expiration_date);
-      return text;
-    },
   },
   data() {
     return {
@@ -72,6 +59,39 @@ export default {
       showWarnDialog: false,
     };
   },
+  computed: {
+    pet() {
+      return this.request.pet;
+    },
+
+    /**
+     * Builds a sentence about expiration with formatted date, based on the pet's sex
+     */
+    expirationText() {
+      let text = this.pet.sexes_id === 1 ? "Compartida" : "Compartido";
+      text += " hasta el " + this.formatDate(this.request.expiration_date);
+      return text;
+    },
+
+    quoteIntro() {
+      return this.professionalName ? 'Tu mensaje a this.professionalName: ' : 'Mensaje: '
+    },
+
+    dialogTitle(){
+      return this.professionalName ? `¿Dejar de compartir a ${this.pet.name}?` : `¿Dejar de ver a ${this.pet.name}?`
+    },
+
+    dialogText(){
+      return this.professionalName ? `${this.professionalName} no podrá ver más el perfil de ${this.pet.name}. Podés volver a compartir la mascota cuando quieras.` : `no podrá ver más el perfil de ${this.pet.name} hasta que el dueño te la vuelva a compartir.`
+    },
+
+    dialogButtonText(){
+      return this.professionalName ? `Dejar de compartir` : `Dejar de ver`
+    },
+  },
+  mounted() {
+
+  }
 };
 </script>
 
