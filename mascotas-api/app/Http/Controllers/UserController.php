f4e9605c6fd6b2521b817350c513900e3b461b97
VroniZinsser <vroni.z@posteo.de>
2022-03-03 04:03:01 +0100

    Corregir edición de usuario profesional

--- mascotas-api/app/Http/Controllers/UserController.php
+++ mascotas-api/app/Http/Controllers/UserController.php
@@ -91,7 +91,7 @@ class UserController extends Controller
             $dto->set_profile_img_id($image->id);
         }
       
-        $user = $this->userRepository->updateOrCreate($dto)->load(['profile_image']);
+        $user = $this->userRepository->updateOrCreate($dto, $user)->load(['profile_image']);
 
         $user->userTypes()->sync($request->get('user_types'));
 
